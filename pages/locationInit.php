a1dc20ee85042d30a45e308d0c91af702c39769f
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2018-09-26 16:22:40 -0300

    avance

--- pages/locationInit.php
+++ pages/locationInit.php
@@ -2,7 +2,7 @@
 require_once(APPPATH . 'widgets/custom/library/dispatcher.php');
 $_REQUEST[Dispatcher::OPTION_PARAM]=Dispatcher::UBICACION_LABEL;
 $_REQUEST[Dispatcher::NO_VOLVER]="";
-require_once(APPPATH . 'widgets/custom/library/telefonica/basicInit.php');
+require_once(APPPATH . 'widgets/custom/library/basicInit.php');
 ?>
 <head>
 </head>
