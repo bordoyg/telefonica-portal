9eecbfed5194be458ce2c35b7dfa1d6a021e1e92
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2018-12-28 14:54:32 -0300

    maqueta etb primer commit

---
+++ js/site-scripts.js
@@ -0,0 +1,63 @@
+var tamHeight = 0;
+var tamWidth = 0;
+
+$(document).ready(function () {
+    Inicio();
+
+    $(window).bind('resize', function () {
+        Inicio();
+    });
+
+    $.datepicker.regional['es'] = {
+        closeText: 'Cerrar',
+        prevText: '<Ant',
+        nextText: 'Sig>',
+        currentText: 'Hoy',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
+        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+    $('#calendar').datepicker({
+        dateFormat: 'dd/mm/yy',
+        showButtonPanel: false,
+        changeMonth: false,
+        changeYear: false,
+        /*showOn: "button",
+        buttonImage: "images/calendar.gif",
+        buttonImageOnly: true,
+        minDate: '+1D',
+        maxDate: '+3M',*/
+        inline: true
+    });
+	
+	
+	$('.ui-state-default').click(function () {
+	   $('.mostrar').css('display','block');
+	});
+
+});
+
+function Inicio() {
+
+    tamWidth = $(window).width();
+    tamHeight = $(window).height();
+
+    $(".content").css('min-height', tamHeight - 70);
+
+    if (tamWidth <= 760) {
+        $("body").addClass("rps");
+    } else {
+        $("body").removeClass("rps");
+    }
+};
+
+
