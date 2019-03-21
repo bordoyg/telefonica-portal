01da339f6551199ba62cd5bb4b66914bd352253b
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2019-03-21 10:46:33 -0300

    fix tecnico en camino 2

--- library/controlador.php
+++ library/controlador.php
@@ -5,42 +5,42 @@ require_once(APPPATH . 'widgets/custom/library/serviceSoap.php');
 class Controlador {
     const MESSAGE_PARAM = 'errorMessage';
     const MESSAGES_MODAL_URL = 'messagesModal.php';
-	const ACTIVITY_PARAM = 'activity';
-	const LOCATION_TECHNICAN_LON_PARAM='locationTechnicanLon';
-	const LOCATION_TECHNICAN_LAT_PARAM='locationTechnicanLat';
-	const LOCATION_CUSTOMER_LON_PARAM='locationCustomerLon';
-	const LOCATION_CUSTOMER_LAT_PARAM='locationCustomerLat';
-	const LOCATION_TECHNICAN="technicanLocation";
-	const SCHUEDULE_DATE_PARAM='timeSlot';
-	const STATUS_LOCALIZABLE=array("onTheWay", "started");
-	const STATUS_VIGENTE=array("onTheWay", "started", "pending");
-	const STATUS_PENDING="pending";
-	
-	const ERROR_GENERIC_MSJ="<h1 class=\"resalt\">ERROR</h1> <p>No fue posible procesar tu solicitud, por favor int&eacute;ntalo m&aacute;s tarde.</p> ";
-	const ERROR_ORDEN_INEXISTENTE="La orden no existe";
-	const ERROR_ORDEN_NO_VIGENTE="La fecha de tu cita con ETB ha expirado. Para volver agendar tu cita comun&iacute;cate al 3777777";
-	const ERROR_REAGENDAR_MSJ="<h1 class=\"resalt\">ERROR DE AGENDAMIENTO</h1> <p>No fue posible reagendar tu cita. Por favor inténtelo más tarde.</p>";
-	
-	const MSJ_ORDEN_CONFIRMADA="<h1>Cita Confirmada</h1><p>Tu cita fue confirmada para el dia </p>@diaCita@<p>¡MUCHAS GRACIAS!</p>";
-	const MSJ_ORDEN_MODIFICADA="<h1>Cita Reagendada</h1><p>La nueva fecha para tu cita es</p>@diaCita@<p>¡MUCHAS GRACIAS!</p>";
+    const ACTIVITY_PARAM = 'activity';
+    const LOCATION_TECHNICAN_LON_PARAM='locationTechnicanLon';
+    const LOCATION_TECHNICAN_LAT_PARAM='locationTechnicanLat';
+    const LOCATION_CUSTOMER_LON_PARAM='locationCustomerLon';
+    const LOCATION_CUSTOMER_LAT_PARAM='locationCustomerLat';
+    const LOCATION_TECHNICAN="technicanLocation";
+    const SCHUEDULE_DATE_PARAM='timeSlot';
+    const STATUS_LOCALIZABLE=array("onTheWay", "started");
+    const STATUS_VIGENTE=array("onTheWay", "started", "pending");
+    const STATUS_PENDING="pending";
+    
+    const ERROR_GENERIC_MSJ="<h1 class=\"resalt\">ERROR</h1> <p>No fue posible procesar tu solicitud, por favor int&eacute;ntalo m&aacute;s tarde.</p> ";
+    const ERROR_ORDEN_INEXISTENTE="La orden no existe";
+    const ERROR_ORDEN_NO_VIGENTE="La fecha de tu cita con ETB ha expirado. Para volver agendar tu cita comun&iacute;cate al 3777777";
+    const ERROR_REAGENDAR_MSJ="<h1 class=\"resalt\">ERROR DE AGENDAMIENTO</h1> <p>No fue posible reagendar tu cita. Por favor inténtelo más tarde.</p>";
+    
+    const MSJ_ORDEN_CONFIRMADA="<h1>Cita Confirmada</h1><p>Tu cita fue confirmada para el dia </p>@diaCita@<p>¡MUCHAS GRACIAS!</p>";
+    const MSJ_ORDEN_MODIFICADA="<h1>Cita Reagendada</h1><p>La nueva fecha para tu cita es</p>@diaCita@<p>¡MUCHAS GRACIAS!</p>";
     const MSJ_ORDEN_CANCELADA="<h1>Cita Cancelada</h1><p>Su cita fue Cancelada</p><h2>@diaCita@</h2><p> s&iacute; requiere agendar una nueva cita por favor comun&iacute;quese a nuestra l&iacute;nea de atenci&oacute;n 3777777</p>";
     const MSJ_CANCELACION_NOPUEDOATENDER="<h1>Cita Cancelada</h1><p>Su cita fue Cancelada</p><h2>@diaCita@</h2><p>Por favor comunicarse con el operador logístico. Línea fija en Bogotá (1)3558170 o al WhatsApp 323-2056558</p>";
-	const MSJ_LIMITE_CANCELACIONES="<h1>Cita no Cancelada</h1><p>Su cita fue Cancelada</p><h2>@diaCita@</h2><p>Llegó al límite de cancelaciones, y entonces los equipos serán cobrados</p>";
-	const MSJ_LIMITE_MODIFICACIONES="<h1>Cita no Reagendada</h1><p>Su cita no fue Reagendada</p><h2>@diaCita@</h2><p>Llegó al límite de reagendamientos, y entonces los equipos serán cobrados</p>";
-	
-	
-	const SUB_STATUS_CANCELADA="CANCELADA";
-	const SUB_STATUS_CONFIRMADA="CONFIRMADA";
-	const SUB_STATUS_MODIFICADA="MODIFICADA";
-	
-	const APROVISIONAMIENTO="APROV";
-	const ASEGURAMIENTO="ASEG";
-	const RECUPERO="REC";
-	
-	const APROVISIONAMIENTO_VALUES=array('INS','COB_INS', 'FTTC_INS');
-	const ASEGURAMIENTO_VALUES=array('MOD','COB_MOD');
-	const RECUPERO_VALUES=array('RET','FTTC_RET');
-	
+    const MSJ_LIMITE_CANCELACIONES="<h1>Cita no Cancelada</h1><p>Su cita fue Cancelada</p><h2>@diaCita@</h2><p>Llegó al límite de cancelaciones, y entonces los equipos serán cobrados</p>";
+    const MSJ_LIMITE_MODIFICACIONES="<h1>Cita no Reagendada</h1><p>Su cita no fue Reagendada</p><h2>@diaCita@</h2><p>Llegó al límite de reagendamientos, y entonces los equipos serán cobrados</p>";
+    
+    
+    const SUB_STATUS_CANCELADA="CANCELADA";
+    const SUB_STATUS_CONFIRMADA="CONFIRMADA";
+    const SUB_STATUS_MODIFICADA="MODIFICADA";
+    
+    const APROVISIONAMIENTO="APROV";
+    const ASEGURAMIENTO="ASEG";
+    const RECUPERO="REC";
+    
+    const APROVISIONAMIENTO_VALUES=array('INS','COB_INS', 'FTTC_INS');
+    const ASEGURAMIENTO_VALUES=array('MOD','COB_MOD');
+    const RECUPERO_VALUES=array('RET','FTTC_RET');
+    
     private $service=NULL;
     private $serviceSoap=NULL;
     
@@ -55,7 +55,7 @@ class Controlador {
         }
         $this->serviceSoap = $GLOBALS['serviceSoap'];
     }
-
+    
     function findTechnicanLocation($activity){
         //Obtenemos la posicion del domicilio
         if(isset($activity->latitude) && isset($activity->longitude)){
@@ -64,22 +64,22 @@ class Controlador {
         }
         
         //Obtenemos la posicion del tecnico
-         $locationData=$this->service->request('/rest/ofscCore/v1/whereIsMyTech', 'GET', 'activityId=' . $activity->activityId . '&includeAvatarImageData=true');
-         $_REQUEST[Controlador::LOCATION_TECHNICAN]=$locationData;
-         
-
-         if(isset($locationData->coordinates) && isset($locationData->coordinates->latitude) && isset($locationData->coordinates->longitude)){
-             $_REQUEST[Controlador::LOCATION_TECHNICAN_LON_PARAM]= $locationData->coordinates->longitude;
-             $_REQUEST[Controlador::LOCATION_TECHNICAN_LAT_PARAM]= $locationData->coordinates->latitude;
-         }else{
-             $this->addMessageError("No se puedo establecer la ubicacion del t&eacute;cnico, intenta mas tarde");
-         }
-
-         return $locationData;
+        $locationData=$this->service->request('/rest/ofscCore/v1/whereIsMyTech', 'GET', 'activityId=' . $activity->activityId . '&includeAvatarImageData=true');
+        $_REQUEST[Controlador::LOCATION_TECHNICAN]=$locationData;
+        
+        
+        if(isset($locationData->coordinates) && isset($locationData->coordinates->latitude) && isset($locationData->coordinates->longitude)){
+            $_REQUEST[Controlador::LOCATION_TECHNICAN_LON_PARAM]= $locationData->coordinates->longitude;
+            $_REQUEST[Controlador::LOCATION_TECHNICAN_LAT_PARAM]= $locationData->coordinates->latitude;
+        }else{
+            $this->addMessageError("No se puedo establecer la ubicacion del t&eacute;cnico, intenta mas tarde");
+        }
+        
+        return $locationData;
     }
     
     function findTechnicanLocationJson(){
-
+        
         $activityID=$this->getActivityIdFromContext();
         $activity=$this->findActivityData($activityID);
         $coordinatesJson="";
@@ -88,23 +88,23 @@ class Controlador {
         /*---DummyData---*/
         $dummy = date('s', time());
         $dummy= $dummy /10;
-
+        
         $dummyX=intval($locationData->coordinates->longitude) +intval($dummy);
         $dummyY=intval($locationData->coordinates->latitude) +intval($dummy);
-
+        
         $dummyX=strval($dummyX);
         $dummyY=strval($dummyY);
-
-
-
-        $coordinatesJson='{'.$dummyX.','.$dummyY.'}';
+        
+        
+        
+        $coordinatesJson="<coordinates><x>".$dummyX."</x><y>".$dummyY."</y></coordinates>";
         /*---End Dummy---*/
-
+        
         // $coordinatesJson='{'.$locationData->coordinates->longitude.','.$locationData->coordinates->latitude.'}';
         
         return $coordinatesJson;
     }
-
+    
     function findAvailabilitySOAP($days) {
         $activityID=$this->getActivityIdFromContext();
         $activity=$this->findActivityData($activityID);
@@ -115,7 +115,7 @@ class Controlador {
         for($i=0; $i<$days - 1; $i++){
             $newDate = strtotime($date."+ 1 days");
             $date = date("Y-m-d",$newDate);
-
+            
             array_push($params, array('date'=>$date));
         }
         
@@ -142,22 +142,22 @@ class Controlador {
                 
             }
         }
-       
+        
         $dates=array();
         $datesAux=array();
         for($i=0; $i<count($response['CAPACITY']); $i++){
             if(isset($response['CAPACITY'][$i]['TIME_SLOT'])
                 && isset($response['CAPACITY'][$i]['AVAILABLE'])){
                     
-                $availableQuota=$response['CAPACITY'][$i]['AVAILABLE'];
-                $activityDuration=$response['ACTIVITY_DURATION'];
-                
-                if($availableQuota>$activityDuration){
-                    if(!isset($datesAux[$response['CAPACITY'][$i]['DATE']])){
-                        $datesAux[$response['CAPACITY'][$i]['DATE']]=array();
+                    $availableQuota=$response['CAPACITY'][$i]['AVAILABLE'];
+                    $activityDuration=$response['ACTIVITY_DURATION'];
+                    
+                    if($availableQuota>$activityDuration){
+                        if(!isset($datesAux[$response['CAPACITY'][$i]['DATE']])){
+                            $datesAux[$response['CAPACITY'][$i]['DATE']]=array();
+                        }
+                        $datesAux[$response['CAPACITY'][$i]['DATE']][$response['CAPACITY'][$i]['TIME_SLOT']]=$timeSlotsMap[$response['CAPACITY'][$i]['TIME_SLOT']];
                     }
-                    $datesAux[$response['CAPACITY'][$i]['DATE']][$response['CAPACITY'][$i]['TIME_SLOT']]=$timeSlotsMap[$response['CAPACITY'][$i]['TIME_SLOT']];
-                }
             }
         }
         
@@ -195,10 +195,10 @@ class Controlador {
                 array_push($dates, $date);
             }
         }
-
+        
         return $dates;
     }
-   
+    
     function findActivityData($activityID){
         try {
             //Buscar los datos de la actividad
@@ -261,7 +261,7 @@ class Controlador {
             return Dispatcher::MESSAGES_URL;
         }
     }
-
+    
     function excecuteNoPuedoAtenderConfirm(){
         try {
             $activityID=$this->getActivityIdFromContext();
@@ -308,9 +308,9 @@ class Controlador {
             return Dispatcher::MESSAGES_URL;
         }
     }
-
+    
     function excecuteScheduleCalendar(){
-
+        
         return Dispatcher::SCHEDULE_DATE_URL;
     }
     function excecuteScheduleConfirmSOAP(){
@@ -374,7 +374,7 @@ class Controlador {
             $params=json_encode($params);
             //Se actualiza el dia
             $this->service->request('/rest/ofscCore/v1/activities/' . $activity->activityId . '/custom-actions/move', 'POST', $params);
-
+            
             if( $detectedAdctivityType != null && strcmp($detectedAdctivityType, Controlador::RECUPERO) ){
                 $modificacionesHechas++;
                 $params["XA_NUM_MOD_PORTAL"] = strval($modificacionesHechas);
@@ -386,7 +386,7 @@ class Controlador {
             $params=json_encode($params);
             //Se actualiza el timeslot y el estado XA_CONFIRMACITA y XA_REAGENDA_PORTAL
             $this->service->request('/rest/ofscCore/v1/activities/' . $activity->activityId, 'PATCH', $params);
-
+            
             $activity=$this->findActivityData($activityID);
             $dateStart = new DateTime($activity->date . ' ' . $activity->serviceWindowStart);
             $dateEnd = new DateTime($activity->date . ' ' . $activity->serviceWindowEnd);
@@ -400,9 +400,9 @@ class Controlador {
             Utils::logDebug('Hubo un error al reagendar la cita', $e);
             $this->addMessageError(Controlador::ERROR_REAGENDAR_MSJ);
             return Dispatcher::MESSAGES_URL;
-        }        
+        }
     }
-
+    
     function excecuteConfirmConfirm(){
         try{
             $activityID=$this->getActivityIdFromContext();
@@ -411,7 +411,7 @@ class Controlador {
             $params=json_encode($params);
             //Se actualiza el estado XA_CONFIRMACITA
             $this->service->request('/rest/ofscCore/v1/activities/' . $activity->activityId, 'PATCH', $params);
-
+            
             $dateStart = new DateTime($activity->date . ' ' . $activity->serviceWindowStart);
             $dateEnd = new DateTime($activity->date . ' ' . $activity->serviceWindowEnd);
             $diaCita= '<h2>' . $dateStart->format('d') . '-' . $GLOBALS['translateMonth'][$dateStart->format('F')] . '-' .$dateStart->format('Y'). '</h2><p>entre las ' . $dateStart->format('g:i A') . ' y las ' . $dateEnd->format('g:i A') . ' hrs.</p>';
@@ -425,7 +425,7 @@ class Controlador {
             $this->addMessageError(Controlador::ERROR_GENERIC_MSJ);
             return Dispatcher::MESSAGES_URL;
         }
-
+        
     }
     function excecuteLocation(){
         $activityID=$this->getActivityIdFromContext();
@@ -469,22 +469,22 @@ class Controlador {
                 || !isset($activity->status)){
                     return false;
             }
-//             $dtCurrent= new DateTime("now");
-//             $dtETA=new DateTime();
-//             $dtETA=$dtETA->createFromFormat("Y-m-d H:i:s", $activity->startTime);
+            //             $dtCurrent= new DateTime("now");
+            //             $dtETA=new DateTime();
+            //             $dtETA=$dtETA->createFromFormat("Y-m-d H:i:s", $activity->startTime);
             
-//             Utils::logDebug("Current: " . $dtCurrent->format("Y-m-d H:i:s"));
-//             Utils::logDebug("dtETA: " . $dtETA->format("Y-m-d H:i:s"));
+            //             Utils::logDebug("Current: " . $dtCurrent->format("Y-m-d H:i:s"));
+            //             Utils::logDebug("dtETA: " . $dtETA->format("Y-m-d H:i:s"));
             
-//             $interval=$dtCurrent->diff($dtETA, false);
-//             $intervalInSeconds = (new DateTime())->setTimeStamp(0)->add($interval)->getTimeStamp();
-//             $intervalInMinutes = $intervalInSeconds/60;
+            //             $interval=$dtCurrent->diff($dtETA, false);
+            //             $intervalInSeconds = (new DateTime())->setTimeStamp(0)->add($interval)->getTimeStamp();
+            //             $intervalInMinutes = $intervalInSeconds/60;
             
-//             Utils::logDebug("interval en minutos: " . $intervalInMinutes);
-//             Utils::logDebug('Estado localizable: ' . in_array($activity->status, Controlador::STATUS_VIGENTE));
-//             Utils::logDebug('XA_ROUTE: ' . (strcmp($activity->XA_ROUTE,"1")==0));
-//             Utils::logDebug('interval mayor a 20: ' . ($intervalInMinutes>=20));
-
+            //             Utils::logDebug("interval en minutos: " . $intervalInMinutes);
+            //             Utils::logDebug('Estado localizable: ' . in_array($activity->status, Controlador::STATUS_VIGENTE));
+            //             Utils::logDebug('XA_ROUTE: ' . (strcmp($activity->XA_ROUTE,"1")==0));
+            //             Utils::logDebug('interval mayor a 20: ' . ($intervalInMinutes>=20));
+            
             $isVigente= ($this->showTechnicanLocation() || $this->showCancel());
             Utils::logDebug('isVigente: ' . ($isVigente));
             if($isVigente){
@@ -518,7 +518,7 @@ class Controlador {
         $activity=$this->findActivityData($activityID);
         $activityDate = DateTime::createFromFormat('Y-m-d', $activity->date, new DateTimeZone($activity->timeZoneIANA));
         $currentDate=DateTime::createFromFormat('Y-m-d', date('Y-m-d'), new DateTimeZone($activity->timeZoneIANA));
- 
+        
         return ($activity->status == Controlador::STATUS_PENDING) && ($activityDate >= $currentDate) && !$this->showTechnicanLocation();
     }
     function showSchedule(){
@@ -570,7 +570,7 @@ class Controlador {
             $cipher->Mode->ID =1;
             $cipher->IV->Value = 'p0r7417313f0n1c4';
             $cipher->KeySize->LookupName = "128_bits";
-            $cipher->Key = 'p0r7417313f0n1c4';    
+            $cipher->Key = 'p0r7417313f0n1c4';
             $cipher->EncryptedText =$this->base64url_decode($encrypted_data_hex.'==');
             
             $cipher->decrypt();
