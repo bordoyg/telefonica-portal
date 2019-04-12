049e9f305ecacb35c3081d9b0131543338b9bce4
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2019-04-12 10:06:12 -0300

    se quitan paths a archivos js que no existian

--- library/location.php
+++ library/location.php
@@ -107,9 +107,7 @@
 
 		</div>
 	</div>
-	<script
-		src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/ie10-viewport-bug-workaround.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
 </body>
 </html>
